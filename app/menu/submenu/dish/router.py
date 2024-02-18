e03b5fffc790bcf2690691923a564eef53836479
Evgeniy <krasnoperoveg@mail.ru>
2024-02-18 21:04:31 +0200

    get dish by id

--- app/menu/submenu/dish/router.py
+++ app/menu/submenu/dish/router.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from fastapi import APIRouter
+from fastapi.exceptions import HTTPException
 
 from app.menu.submenu.dish.dao import DishDAO
 from app.menu.submenu.dish.schemas import SDish, CreateSDish
@@ -22,7 +25,15 @@ async def create_dish(menu_id: str, submenu_id: str, data: CreateSDish) -> SDish
     return dish
 
 
-@router.get("/{menu_id}/submenus/{submenu_id}/dishes")
-async def get_dishes(menu_id: str, submenu_id: str):
+@router.get("/{menu_id}/submenus/{submenu_id}/dishes", status_code=200)
+async def get_dishes(menu_id: str, submenu_id: str) -> List[SDish]:
     dishes = await DishDAO.get_dishes(submenu_id)
     return dishes
+
+
+@router.get("/{menu_id}/submenus/{submenu_id}/dishes/{dish_id}", status_code=200)
+async def get_dish_by_id(menu_id: str, submenu_id: str, dish_id: str) -> SDish:
+    dish = await DishDAO.find_by_id(dish_id)
+    if not dish:
+        raise HTTPException(status_code=404, detail="dish not found")
+    return dish
